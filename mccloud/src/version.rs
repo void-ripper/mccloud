cc5c486ca0a9127509f762b4e98b49eb00a80c13
void <gojira83@gmx.de>
2025-05-21 22:30:35 +0200

    only use cargo version for mccloud version

--- mccloud/src/version.rs
+++ mccloud/src/version.rs
@@ -7,9 +7,6 @@ pub struct Version {
     major: u16,
     minor: u16,
     patch: u16,
-    target: String,
-    branch: String,
-    commit: String,
 }
 
 impl Default for Version {
@@ -18,9 +15,6 @@ impl Default for Version {
             major: env!("CARGO_PKG_VERSION_MAJOR").parse().unwrap(),
             minor: env!("CARGO_PKG_VERSION_MINOR").parse().unwrap(),
             patch: env!("CARGO_PKG_VERSION_PATCH").parse().unwrap(),
-            target: env!("TARGET").into(),
-            branch: env!("BRANCH").into(),
-            commit: env!("COMMIT").into(),
         }
     }
 }
@@ -35,10 +29,6 @@ impl PartialEq for Version {
 
 impl Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "v{}.{}.{} {} {} {}",
-            self.major, self.minor, self.patch, self.branch, self.commit, self.target
-        )
+        write!(f, "v{}.{}.{}", self.major, self.minor, self.patch,)
     }
 }
